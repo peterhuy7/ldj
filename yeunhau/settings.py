bac0a47d2c983c6facc5a51d5e3337c5a57e022f
peterhuy7 <116619990+peterhuy7@users.noreply.github.com>
2023-08-04 19:00:03 +0700

    Update settings.py

--- yeunhau/settings.py
+++ yeunhau/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=+i01936q2_ja*u2sp-&_@tnnihy#a@pp$9lq(uerpws+hhkz0
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tn77777.domcloud.com',]
 
 
 # Application definition
